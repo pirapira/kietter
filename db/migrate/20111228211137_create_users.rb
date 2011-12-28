cfb318f72fbc57a1fc3b729c62da40d9db474afd
kietter <kietter@m.pira.jp>
2011-12-29 06:29:40 +0900

    user table

--- db/migrate/20111228211137_create_users.rb
+++ db/migrate/20111228211137_create_users.rb
@@ -1,7 +1,12 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-
+      t.string   "provider",    :null => false
+      t.integer  "uid",         :null => false
+      t.string   "screen_name", :null => false
+      t.string   "name",        :null => false
+      t.text     "token",       :null => false
+      t.text     "secret",      :null => false
       t.timestamps
     end
   end
