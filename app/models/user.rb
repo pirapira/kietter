23a7a6bd0908feb090efb508054803deef59b00d
kietter <kietter@m.pira.jp>
2011-12-29 06:58:14 +0900

    preparing User methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,25 @@
 class User < ActiveRecord::Base
+  validates_uniqueness_of :uid, :scope => :provider
+  def self.create_with_omniauth(auth)
+    create! do |user|
+      user.set_by(auth)
+      Name.add_name(user.screen_name)
+    end
+  end
+  def self.find_existing(auth)
+    ret = User.find_by_provider_and_uid(auth["provider"], auth["uid"])
+    if ret
+      ret.set_by(auth)
+      ret.save! # changing screen name and so on
+    end
+    return ret
+  end
+  def set_by(auth)
+    self.provider = auth["provider"]
+    self.uid = auth["uid"]
+    self.name = auth["user_info"]["name"]
+    self.screen_name = auth["user_info"]["nickname"]
+    self.token = auth['credentials']['token']
+    self.secret = auth['credentials']['secret']
+  end
 end
