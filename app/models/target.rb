f8c9012f0afdd3084e3e949bf824a0436b4f19ae
Yoichi Hirai <kietter@pira.jp>
2011-12-30 02:45:55 +0900

    Target.find_or_create

--- app/models/target.rb
+++ app/models/target.rb
@@ -1,3 +1,12 @@
 class Target < ActiveRecord::Base
+  validates_uniqueness_of :uid
   has_many :samples
+  def Target.find_or_create(uid)
+    ret = find(:first, :conditions => ["uid = ?", uid])
+    return ret if ret
+    ret = Target.new
+    ret.uid = uid
+    return ret if ret.save
+    Target.find_or_create(uid)
+  end
 end
