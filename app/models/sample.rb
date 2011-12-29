5b88159fca99201038fafea913ab345d7b2a5578
Yoichi Hirai <kietter@pira.jp>
2011-12-30 02:28:48 +0900

    filling samples table using array of datetime

--- app/models/sample.rb
+++ app/models/sample.rb
@@ -1,3 +1,22 @@
 class Sample < ActiveRecord::Base
+  require 'set'
   belongs_to :target
+  def Sample.fill(target, arr)
+    return if arr == []
+    arr.each {|t| t.utc}
+    arr = arr.collect {|t| t.soroe}
+    arr.sort!
+    s = Set.new arr
+    kara = arr[0]
+    made = arr[-1]
+    tmp  = kara
+    while tmp <= made && tmp <= Time.now
+      samp = Sample.new
+      samp.target_id = target.id
+      samp.period = tmp
+      samp.presence = s.include? tmp
+      samp.save
+      tmp += Time.tanni
+    end
+  end
 end
