05d57ccfbb7e8aedeed4a298a4f0ae241e390e75
kietter <kietter@m.pira.jp>
2011-12-29 20:35:41 +0900

    session controlling to be used by omniauth

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,2 +1,16 @@
 class SessionController < ApplicationController
+  def callback
+    auth = request.env["omniauth.auth"]
+    user = User.find_existing(auth)
+    unless user
+      user = User.create_with_omniauth(auth)
+    end
+    session[:user_id] = user.id
+    redirect_to root_url, :notice => "Signed in!"
+  end
+
+  def destroy
+    session.clear
+    redirect_to :back, :notice => "Signed out!"
+  end
 end
