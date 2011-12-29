3056dce859a8bc4147a5a99c728d7b35066d1aff
Yoichi Hirai <kietter@pira.jp>
2011-12-30 02:54:24 +0900

    local variable is enough

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,21 +3,21 @@ class HomeController < ApplicationController
   end
   def investigate
     if session[:a] || session[:b]
-      @a = session[:a]
-      @b = session[:b]
+      a = session[:a]
+      b = session[:b]
       session[:a] = session[:b] = nil
     else 
-      @a = params[:a]
-      @b = params[:b]
+      a = params[:a]
+      b = params[:b]
     end
     unless current_user
-      session[:a] = @a
-      session[:b] = @b
+      session[:a] = a
+      session[:b] = b
       redirect_to "/auth/twitter"
     end
     # now logged in
     c = current_user.client
-    @a_user = c.user @a
-    @b_user = c.user @b
+    a_user = c.user a
+    b_user = c.user b
   end
 end
