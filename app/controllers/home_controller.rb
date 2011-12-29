a6c5f80b70eee12cb69020c228e3056611552328
kietter <kietter@m.pira.jp>
2011-12-29 23:54:02 +0900

    accept two inputs and display them

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,19 @@ class HomeController < ApplicationController
   def index
   end
   def investigate
+    if session[:a] || session[:b]
+      @a = session[:a]
+      @b = session[:b]
+      session[:a] = session[:b] = nil
+    else 
+      @a = params[:a]
+      @b = params[:b]
+    end
     return if session[:user_id]
+
     # not logged in
+    session[:a] = @a
+    session[:b] = @b
     redirect_to "/auth/twitter"
     return
   end
